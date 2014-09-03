8a1f773948288c9bdf6accdba79d27a701bbeaf5
Mark Chavez <markchav3z@gmail.com>
2014-09-03 14:27:15 +0800

    for publish

--- lib/direct_sms.rb
+++ lib/direct_sms.rb
@@ -8,35 +8,23 @@ module DirectSms
     base_uri "api.directsms.com.au"
     attr_accessor :message, :to, :max_segments, :type, :message_id
 
-    def initialize(username, password, options = {})
-      @username = username
-      @password = password
-
-      options.each { |k, v| self.send("#{k}=", v) }
+    def initialize(username, password, attrs = {})
+      @options = { username: username, password: password }
+      attrs.each { |k, v| self.send("#{k}=", v) }
     end
     
     def balance
-      options = {
-        query: {
-          username: @username,
-          password: @password
-        }
-      }
-      
+      options = { query: @options }
       self.class.get("/s3/http/get_balance", options)
     end
 
     def send_message
       options = {
-        query: {
-          username: @username,
-          password: @password,
-          message: @message,
-          to: @to,
-          type: @type,
-          max_segments: @max_segments,
-          messageid: @message_id
-        }
+        query: @options.merge(message: @message,
+                              to: @to,
+                              max_segments: @max_segments, 
+                              type: @type,
+                              messageid: @message_id)
       }
 
       self.class.get("/s3/http/send_message", options)
