e7c608c6be5f270f69aec20a1082cc5a9bcc009d
Mark Chavez <markchav3z@gmail.com>
2014-09-03 12:05:54 +0800

    direct sms exec

--- spec/lib/direct_sms_spec.rb
+++ spec/lib/direct_sms_spec.rb
@@ -5,15 +5,17 @@ describe DirectSms do
   let!(:username) { "username" }
   let!(:password) { "password" }
 
-  describe 'configuration' do
+  describe "balance" do
     it 'returns the remaining balance' do
       stub_request(:get, "http://api.directsms.com.au/s3/http/get_balance?password=#{password}&username=#{username}").
-        to_return(:status => 200, :body => "credit: 300.00", :headers => {})
+      to_return(:status => 200, :body => "credit: 300.00", :headers => {})
 
       sms = DirectSms::Message.new(username, password)
       expect(sms.balance.body).to eq "credit: 300.00"
     end
+  end
 
+  describe "sending sms" do
     it 'sends an sms message' do
       stub_request(:get, "http://api.directsms.com.au/s3/http/send_message?max_segments=10&message=This%20is%20a%20test&password=password&to=%2B639178574111&type=2-way&username=username").
         to_return(:status => 200, :body => "id: 1234567890", :headers => {})
