4cebc1a37c0740b52aa61cb542f867bb674e8c3e
Mark Chavez <markchav3z@gmail.com>
2014-09-30 13:31:01 +0800

    minor api change

--- lib/direct_sms.rb
+++ lib/direct_sms.rb
@@ -7,27 +7,33 @@ module DirectSms
 
     base_uri "api.directsms.com.au"
     attr_accessor :message, :to, :max_segments, :type, :message_id
+    attr_accessor :username, :password
 
-    def initialize(username, password, attrs = {})
-      @options = { username: username, password: password }
+    def initialize(attrs = {})
       attrs.each { |k, v| self.send("#{k}=", v) }
     end
     
     def balance
-      options = { query: @options }
+      options = { query: fetch_credentials }
       self.class.get("/s3/http/get_balance", options)
     end
 
     def send_message
       options = {
-        query: @options.merge(message: @message,
-                              to: @to,
-                              max_segments: @max_segments, 
-                              type: @type,
-                              messageid: @message_id)
+        query: fetch_credentials.merge(message: message,
+                              to: to,
+                              max_segments: max_segments, 
+                              type: type,
+                              messageid: message_id)
       }
 
       self.class.get("/s3/http/send_message", options)
+    end
+
+    private
+
+    def fetch_credentials
+      { username: username, password: password }
     end
   end
 end
--- spec/lib/direct_sms_spec.rb
+++ spec/lib/direct_sms_spec.rb
@@ -10,14 +10,14 @@ describe DirectSms do
       stub_request(:get, "http://api.directsms.com.au/s3/http/get_balance?password=#{password}&username=#{username}").
       to_return(:status => 200, :body => "credit: 300.00", :headers => {})
 
-      sms = DirectSms::Message.new(username, password)
+      sms = DirectSms::Message.new(username: username, password: password)
       expect(sms.balance.body).to eq "credit: 300.00"
     end
   end
 
   describe "sending sms" do
     it 'attributes can be set on new' do
-      sms = DirectSms::Message.new(username, password, :type => "2-way", :max_segments => 20)
+      sms = DirectSms::Message.new(username: username, password: password, :type => "2-way", :max_segments => 20)
 
       expect(sms.max_segments).to eq 20
       expect(sms.type).to eq "2-way"
@@ -27,7 +27,7 @@ describe DirectSms do
       stub_request(:get, "http://api.directsms.com.au/s3/http/send_message?max_segments=10&message=This%20is%20a%20test&password=password&to=%2B639178574111&messageid=&type=2-way&username=username").
         to_return(:status => 200, :body => "id: 1234567890", :headers => {})
 
-      sms = DirectSms::Message.new(username, password, :type => "2-way", :max_segments => 10)
+      sms = DirectSms::Message.new(username: username, password: password, :type => "2-way", :max_segments => 10)
       sms.message = "This is a test"
       sms.to = "+639178574111"
 
